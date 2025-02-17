fc1b60d14cb6edaba589e722845ef71cfdb52205
begumkucuk35 <35begumkucuk@gmail.com>
2025-02-17 23:01:30 +0300

    Project created with next.js and aceternity ui spotlight and grid added

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100">
+      <div>
+        <Hero/>
+      </div>
+    </main>
+  );
+}
