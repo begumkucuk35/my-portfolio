22baec842909a12ffde3fb1dbbc825f39714b4b5
begum_kucuk9 <begumkucuk9@izmir.bel.tr>
2025-02-21 15:48:51 +0300

    div tag fix

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -65,68 +65,67 @@ export const BentoGridItem = ({
             />
           )}
         </div>
-      </div>
-      <div
-        className={`absolute right-0 -bottom-5 ${
-          id === 5 && "w-full opacity-80"
-        } `}
-      >
-        {spareImg && (
-          <img
-            src={spareImg}
-            alt={spareImg}
-            className="object-cover object-center w-full h-full"
-          />
-        )}
-      </div>
-      {id == 6 && (
-        /*Aceternity UI Background Gradient Animation*/
-        <BackgroundGradientAnimation>
-          <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
-        </BackgroundGradientAnimation>
-      )}
-      <div
-        className={cn(
-          titleClassName,
-          "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
-        )}
-      >
-        <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
-          {description}
+        <div
+          className={`absolute right-0 -bottom-5 ${
+            id === 5 && "w-full opacity-80"
+          } `}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className="object-cover object-center w-full h-full"
+            />
+          )}
         </div>
-        <div className="font-sans font-bold text-lg lg:text-3xl text-white max-w-96 z-10">
-          {title}
-        </div>
-
-        {id === 3 && (
-          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-              {leftLists.map((item, i) => (
-                <span
-                  key={i}
-                  className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-                >
-                  {item}
-                </span>
-              ))}
-              <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
-            </div>
-            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-              <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
-              {rightLists.map((item, i) => (
-                <span
-                  key={i}
-                  className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-                >
-                  {item}
-                </span>
-              ))}
-            </div>
+        {id === 6 && (
+          /*Aceternity UI Background Gradient Animation*/
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+          </BackgroundGradientAnimation>
+        )}
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
+        >
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+            {description}
           </div>
-        )}
-        
+          <div className="font-sans font-bold text-lg lg:text-3xl text-white max-w-96 z-10">
+            {title}
+          </div>
+
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                {leftLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              </div>
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                {rightLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
